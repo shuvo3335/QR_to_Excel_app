b104f369938642c9c631e6f7ebaf402ccc98a19c
Nayem Faisal <30442173+shuvo3335@users.noreply.github.com>
2023-10-02 12:28:52 +0600

    Text duplicity updated

--- app/src/main/java/com/example/qrapplication/IntroSlider.java
+++ app/src/main/java/com/example/qrapplication/IntroSlider.java
@@ -39,9 +39,7 @@ public class IntroSlider extends AppCompatActivity {
         }
 
         // Making notification bar transparent
-        if (Build.VERSION.SDK_INT >= 21) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         setContentView(R.layout.activity_intro_slider);
 
         viewPager = findViewById(R.id.view_pager);
@@ -134,11 +132,9 @@ public class IntroSlider extends AppCompatActivity {
     // Making notification bar transparent
 
     private void changeStatusBarColor() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
-        }
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.TRANSPARENT);
     }
 
     /**
--- app/src/main/java/com/example/qrapplication/MainActivity.java
+++ app/src/main/java/com/example/qrapplication/MainActivity.java
@@ -1,16 +1,12 @@
 package com.example.qrapplication;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import java.io.File;
@@ -20,26 +16,33 @@ import java.io.IOException;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import java.util.HashSet;
+import java.util.Set;
+import android.Manifest;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.content.pm.PackageManager;
+import android.util.Log;
+import android.view.View;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.annotation.NonNull;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import java.io.OutputStream;
+import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashSet;
-import java.util.Set;
-
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import javax.xml.stream.XMLInputFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int CAMERA_PERMISSION_REQUEST_CODE = 200;
     private TextView resultTextView;
-    private int currentRowIndex = 0, num = 0; // Initialize the current row index
-
+    private int currentRowIndex = 0; // Initialize the current row index
+    private static final int DIRECTORY_REQUEST_CODE =100;
     private Set<String> scannedQR = new HashSet<>();
 
     @Override
@@ -49,13 +52,35 @@ public class MainActivity extends AppCompatActivity {
 
         resultTextView = findViewById(R.id.resultTextView);
         Button scanButton = findViewById(R.id.scanButton);
+        Button fileViewBtn = findViewById(R.id.fileBtn);
 
-        scanButton.setOnClickListener(view -> {
+        fileViewBtn.setOnClickListener(view -> {
+            String folderPath = getExternalFilesDir(null)+ "/QR Scan Data";
+            // Create a Uri for the folder path
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            Uri uri = Uri.parse(folderPath);
+            intent.setDataAndType(uri, "*/*");
+
+            if(intent.resolveActivity(getPackageManager())!=null){
+                startActivity(intent);
+            }
+            else {
+                Toast.makeText(this, "No file Manager App found", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        scanButton.setOnClickListener( view -> {
+            startQRScanner();
+        });
+
+        /*scanButton.setOnClickListener(view -> {
             // Check and request camera and storage permissions if not granted
             if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
                     != PackageManager.PERMISSION_GRANTED ||
                     ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                            != PackageManager.PERMISSION_GRANTED) {
+                            != PackageManager.PERMISSION_GRANTED)
+            {
 
                 ActivityCompat.requestPermissions(MainActivity.this,
                         new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
@@ -63,10 +88,11 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 // Start QR code scanner
                 startQRScanner();
-            }
-        });
-    }
+                Toast.makeText(this, "button pressed", Toast.LENGTH_SHORT).show();
 
+            }
+        });*/
+    }
     private void startQRScanner() {
         IntentIntegrator integrator = new IntentIntegrator(this);
         integrator.setOrientationLocked(true);
@@ -85,12 +111,12 @@ public class MainActivity extends AppCompatActivity {
                 if (!scannedQR.contains(qrData)){
                     scannedQR.add(qrData);
                     resultTextView.setText(qrData);
-
                     // Parse QR data and write it to an Excel file
                     writeToExcel(qrData);
                     currentRowIndex++;
                 }
                 else {
+                    resultTextView.setText("SCAN NEW QR CODE TO GET DATA");
                     Toast.makeText(this, "THIS QR CODE IS ALREADY SCANNED", Toast.LENGTH_LONG).show();
                 }
 
@@ -99,24 +125,27 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "No QR data found to write", Toast.LENGTH_SHORT).show();
             }
         }
+
         else
         {
             super.onActivityResult(requestCode, resultCode, data);
         }
+
     }
+
+
 
     private void writeToExcel(String qrData) {
         // Define the file path
         //String filePath = getFilesDir() + "/qr_data.xls";
         String currentDate = java.time.LocalDate.now().toString();
         String folderName = "QR Scan Data";
-        String fileName = "qr_data"+ currentDate +".xls";
+        String fileName = "qr_data" + currentDate + ".xls";
 
         File internalStorageDir = getExternalFilesDir(null);
 
         File folder = new File(internalStorageDir, folderName);
-        if (!folder.exists())
-        {
+        if (!folder.exists()) {
             folder.mkdir();
             Toast.makeText(this, "directory created", Toast.LENGTH_SHORT).show();
         }
@@ -130,66 +159,77 @@ public class MainActivity extends AppCompatActivity {
         if (file.exists()) {
             try (FileInputStream fis = new FileInputStream(file)) {
 
-                //Disable namespace awareness for XML parsing
-                /*XMLInputFactory factory = XMLInputFactory.newInstance();
-                factory.setProperty(XMLInputFactory.IS_NAMESPACE_AWARE, false);*/
+                // Disable namespace awareness for XML parsing
                 workbook = WorkbookFactory.create(fis);
 
                 // Open the existing Excel file
-                Log.d("File exist tag", "writeToExcel: file existing error");
-//                workbook = new XSSFWorkbook();
                 sheet = workbook.getSheetAt(0); // Assuming you have only one sheet
 
-                // Find the last row with data and increment the currentRowIndex
-                int lastRowNum = sheet.getLastRowNum();
-                currentRowIndex = lastRowNum+1;
+                // Check if the QR data is already in the HashSet
+                if (!scannedQR.contains(qrData)) {
+                    // Find the last row with data and increment the currentRowIndex
+                    int lastRowNum = sheet.getLastRowNum();
+                    currentRowIndex = lastRowNum + 1;
+
+                    // Create a new row for each QR code scan and set the data in the corresponding cells
+                    Row row = sheet.createRow(currentRowIndex);
+
+                    // Split QR data by whitespace and write to separate cells in the row
+                    String[] dataParts = qrData.split(" ");
+                    for (int i = 0; i < dataParts.length; i++) {
+                        Cell cell = row.createCell(i);
+                        cell.setCellValue(dataParts[i]);
+                    }
+
+                    // Add the QR data to the HashSet to mark it as scanned
+                    scannedQR.add(qrData);
+
+                    // Save the workbook to the file
+                    try (FileOutputStream fos = new FileOutputStream(filePath)) {
+                        workbook.write(fos);
+                        resultTextView.append("\nData written to Excel file");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    // Handle the case when the QR data is a duplicate
+                    resultTextView.setText("SCAN NEW QR CODE TO GET DATA");
+                    Toast.makeText(this, "THIS QR CODE IS ALREADY SCANNED", Toast.LENGTH_LONG).show();
+                }
             } catch (IOException e) {
                 e.printStackTrace();
                 return;
             }
-        }
-
-        else {
+        } else {
             // Create a new Excel workbook and sheet if the file doesn't exist
             workbook = new HSSFWorkbook();
-            sheet = workbook.createSheet("QR Data"); //+" "+ num++
-        }
+            sheet = workbook.createSheet("QR Data");
+            // Create a new row for each QR code scan and set the data in the corresponding cells
+            Row row = sheet.createRow(currentRowIndex);
 
-        // Create a new row for each QR code scan and set the data in the corresponding cells
-        Row row = sheet.createRow(currentRowIndex);
-
-        // Split QR data by whitespace and write to separate cells in the row
-        String[] dataParts = qrData.split(" ");
-        for (int i = 0; i < dataParts.length; i++) {
-            Cell cell = row.createCell(i);
-            cell.setCellValue(dataParts[i]);
-        }
-
-            /*try (OutputStream outputStream = new FileOutputStream(filePath))
-            {
-                workbook.write(outputStream);
-                Toast.makeText(this, "error occure here", Toast.LENGTH_SHORT).show();
-                // Create a new row for each QR code scan and set the data in the corresponding cells
-                Row row = sheet.createRow(2);
-                currentRowIndex++;
-                // Split QR data by whitespace and write to separate cells in the row
-                String[] dataParts = qrData.split(" ");
-                for (int i = 0; i < dataParts.length; i++) {
-                    Cell cell = row.createCell(i);
-                    cell.setCellValue(dataParts[i]);
+            // Split QR data by whitespace and write to separate cells in the row
+            String[] dataParts = qrData.split(" ");
+            for (int i = 0; i < dataParts.length; i++) {
+                Cell cell = row.createCell(i);
+                cell.setCellValue(dataParts[i]);
             }
-        } catch (IOException e) {
-                throw new RuntimeException(e);
-            }*/
 
+            // Add the QR data to the HashSet to mark it as scanned
+            scannedQR.add(qrData);
 
-        // Save the workbook to the file
-        try (FileOutputStream fos = new FileOutputStream(filePath)) {
-            workbook.write(fos);
-            resultTextView.append("\nData written to Excel file");
-        } catch (IOException e) {
-            e.printStackTrace();
+            // Save the workbook to the file
+            try (FileOutputStream fos = new FileOutputStream(filePath)) {
+                workbook.write(fos);
+                resultTextView.append("\nData written to Excel file");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // Increment the currentRowIndex
+            currentRowIndex++;
         }
     }
+
+
 
 }
